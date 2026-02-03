b2c339a3f98ecdc5b5430b2541b931269291d720
VladSakas <vlad.sakas@yandex.ru>
2026-02-03 18:26:57 +0200

    переписал toString, equals, hashcode

---
+++ src/people/Person.java
@@ -0,0 +1,40 @@
+package people;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private String surname;
+    private Gender gender;
+
+    public Person(String name, String surname, Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " " + getSurname();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && gender == person.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, gender);
+    }
+}
